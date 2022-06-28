fb89bf729ba931422738425e840a79ca3f1e02cd
maxembo <91334358+maxembo@users.noreply.github.com>
2022-06-28 17:02:33 +0300

    add Example1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Example
+{
+    internal class Example1
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Popov Maxim Romanovich", email = "maxim.popov1234@mail.ru";
+            double progrScores = 65.6, mathScores = 78.3, physicsScores = 59.1;
+            int age = 20;
+
+            Console.WriteLine($"ФИО: {fullName}\n" +
+                              $"Возраст: {age}\n" +
+                              $"Email: {email}\n" +
+                              $"Баллы по программированию: {progrScores}\n" +
+                              $"Баллы по математике: {mathScores}\n" +
+                              $"Баллы по физике: {physicsScores}");
+
+        }
+    }
+}
